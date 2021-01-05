ab2c45e8073dfca81eda4c78c30ae77587de5e49
yusuf <ysfozturk3@gmail.com>
2021-01-05 16:27:52 +0100

    static folder

--- src/blog/models.py
+++ src/blog/models.py
@@ -9,6 +9,13 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
+        
+    
 
 
 
@@ -19,11 +26,15 @@ class Post(models.Model):
     )
     title =  models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to = '')
     category=models.ForeignKey(Category,on_delete=models.PROTECT)
     publish_date=models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     status = models.CharField(max_length=10,choices=OPTIONS,default='d')
     slug = models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
+        
     
--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MED_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media_root'
