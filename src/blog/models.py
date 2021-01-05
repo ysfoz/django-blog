bbb6114587b9779bedfe66d7969af1412766e70d
yusuf <ysfozturk3@gmail.com>
2021-01-05 16:10:58 +0100

    created post model

---
+++ src/blog/models.py
@@ -0,0 +1,29 @@
+from os import name
+from django.db import models
+from django.db.models.fields import CharField
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+
+class Post(models.Model):
+    OPTIONS = (
+        ('d','Draft'),
+        ('p','Published')
+    )
+    title =  models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    category=models.ForeignKey(Category,on_delete=models.PROTECT)
+    publish_date=models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    status = models.CharField(max_length=10,choices=OPTIONS,default='d')
+    slug = models.SlugField(blank=True, unique=True)
+    
