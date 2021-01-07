223c621ea819bf4489ea4107f46867485b394afe
yusuf <ysfozturk3@gmail.com>
2021-01-07 17:55:27 +0100

    added style for template

--- src/blog/models.py
+++ src/blog/models.py
@@ -34,6 +34,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def comment_count(self):
+        return self.comment_set.all().count()
+    
+    def view_count(self):
+        return self.postview_set.all().count()
+    
+    def like_count(self):
+        return self.like_set.all().count()
 
 
 class Comment(models.Model):
--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media_root"
