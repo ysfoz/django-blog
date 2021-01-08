ba53191f7a09d99d96348911374ae2e755b4a340
yusuf <ysfozturk3@gmail.com>
2021-01-08 13:54:12 +0100

    like method

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Post
-from .forms import PostForm
+from .models import Post, Like
+from .forms import PostForm, CommentForm
 
 
 def post_list(request):
@@ -20,7 +20,6 @@ def post_create(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            # form.save()
             return redirect("blog:list")
     context = {
         'form': form
@@ -29,8 +28,17 @@ def post_create(request):
 
 def post_detail(request,slug):
     obj = get_object_or_404(Post,slug=slug)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.post = obj
+        comment.save()
+        return redirect('blog:detail',slug=slug)
+        # return redirect(request.path) bu da ayni sonucu verir
     context = {
-        'object':obj
+        'object':obj,
+        'form':form
     }
     return render(request,'blog/post_detail.html',context)
 
@@ -56,5 +64,26 @@ def post_delete(request,slug):
     }
     return render(request, 'blog/post_delete.html',context)
     
+def like(request,slug):
+    if request.method == 'POST':
+        obj = get_object_or_404(Post,slug=slug)
+        like_qs =Like.objects.filter(user=request.user,post=obj)
+        if like_qs:
+            like_qs[0].delete()
+            
+        else:
+            Like.objects.create(user=request.user,post = obj)
+        return redirect('blog:detail', slug=slug)
+        
     
     
+    
+    
+    
+    
+    # todo bunu deneyelim
+    # if like_qs:
+    #     like_qs = not like_qs 
+    
+    
+   
