792183d9f5845276ad3541e88f259d4ba4214b3c
yusuf <ysfozturk3@gmail.com>
2021-01-10 23:13:54 +0100

    password reset form

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordResetForm, UserCreationForm
 from .models import Profile
 
 class RegistrationForm(UserCreationForm):
@@ -27,4 +27,11 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+        
+class PasswordResetEmailCheck(PasswordResetForm):
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email = email).exists():
+            raise forms.ValidationError('There is no email')
+        return email
         
